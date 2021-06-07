10e0d241273d0c7f1f5a03a6ff8ae8f58fb0a5ac
ABD EL HALIM <abdelhalim.diab@univ-constantine2.dz>
2021-06-07 12:27:32 +0100

    issue done. closes #15

--- src/com/drronidz/Main.java
+++ src/com/drronidz/Main.java
@@ -5,20 +5,47 @@ package com.drronidz;/*
     CREATED ON : 11:39 AM
 */
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class Main {
     public static void main(String[] args) {
 
         // New format
         try (Connection connection = DriverManager.getConnection("jdbc:sqlite:C:\\Work\\Databases\\test.db")){
+//            connection.setAutoCommit(false);
             // Class.forName("org.sql.JDBC");
             Statement statement = connection.createStatement();
             statement.execute("CREATE TABLE IF NOT EXISTS contacts " +
                     "(name TEXT, phone INTEGER, email TEXT)");
+
+//            statement.execute("INSERT INTO contacts (name, phone, email) " +
+//                    "VALUES ('Joe', 12654, 'joe@gmail.com')");
+//
+//            statement.execute("INSERT INTO contacts (name, phone, email) " +
+//                    "VALUES ('Jane', 654321, 'jane@gmail.com')");
+//
+//            statement.execute("INSERT INTO contacts (name, phone, email) " +
+//                    "VALUES ('Fido', 112233, 'fido@gmail.com')");
+//
+//            statement.execute("INSERT INTO contacts (name, phone, email) " +
+//                    "VALUES ('Abdou', 332211, 'abdou@gmail.com')");
+
+//            statement.execute("UPDATE contacts SET phone=123654 WHERE name='Joe'");
+//
+//            statement.execute("DELETE FROM contacts WHERE name ='TIM'");
+
+
+            statement.execute("SELECT * FROM contacts");
+            ResultSet resultSet = statement.getResultSet();
+
+            while (resultSet.next()) {
+                System.out.println(
+                          resultSet.getString("name") + " "
+                        + resultSet.getInt("phone") + " "
+                        + resultSet.getString("email"));
+            }
+            resultSet.close();
+
         } catch (SQLException e) {
             e.printStackTrace();
             System.out.println("Something went wrong: " + e.getMessage());
